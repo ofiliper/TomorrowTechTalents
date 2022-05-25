877ad7e3e476f85a774c6f71058fa453cfe820af
ofilipericardo <62399585+ofiliper@users.noreply.github.com>
2022-05-25 19:40:30 -0300

    Update Subtracao.java

--- POO2_ex1_Calculadora/src/Calculadora/Subtracao.java
+++ POO2_ex1_Calculadora/src/Calculadora/Subtracao.java
@@ -1,11 +1,14 @@
 class Subtracao extends Calculo{
     @Override
     public void exibirResultado() {
-        total = 0d;
+        total = valores.get(0);
         System.out.println("Os valores digitados foram:");
-        for( double valor : this.valores){
-            System.out.println(valor);
-            total -= valor;
+        System.out.println(valores.get(0));
+        for ( int i = 1 ; i < valores.size() ; i++){
+            System.out.println(valores.get(i));
+            if(i > 0){
+                total -= valores.get(i);
+            }
         }
         System.out.println("O total da subtração é: " + total);
     }
